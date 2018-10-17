f4eac9199e498789fdc497471f21892e72b69046
Amélie Barré <amliebarre@gmail.com>
2018-10-17 17:00:22 +0200

    create server

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/pokedex');
+
+app.get('/', function(req, res) {
+    console.log(req);
+    res.send('Pokedex is working');
+});
+
+console.log('App running on port 3000');
+app.listen(3000);
