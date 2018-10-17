16c0b9535c686aca57f5ab43d918bc0205733274
Amélie Barré <amliebarre@gmail.com>
2018-10-17 18:17:31 +0200

    create routes

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+// Connection to database
 mongoose.connect('mongodb://localhost/pokedex');
 
+// Import models
+require('./src/models/Pokemon');
+require('./src/models/Type');
+
+// Default route
 app.get('/', function(req, res) {
     console.log(req);
     res.send('Pokedex is working');
 });
+
+// Routes
+app.use('/pokemons', require('./src/routes/pokemon'));
+app.use('/types', require('./src/routes/type'));
 
 console.log('App running on port 3000');
 app.listen(3000);
