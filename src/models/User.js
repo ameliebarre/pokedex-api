b9dd3fd71ed7b14312ed7093d4689870c05a8b2d
Amélie Barré <amliebarre@gmail.com>
2018-10-23 13:57:51 +0200

    create relation bewteen user and pokemons

--- src/models/User.js
+++ src/models/User.js
@@ -1,32 +1,34 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
-   name: {
-      type: String,
-      trim: true,
-      required: true
-   },
-   email: {
-      type: String,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      required: true
-   },
-   password: {
-      type: String,
-      required: true
-   },
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     permissions: {
         type: Array,
         required: true,
         default: ['USER']
     },
-    trainer: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+    trainer: [{ type: Schema.Types.ObjectId, ref: 'Trainer' }],
+    pokemons: [{ type: Schema.Types.ObjectId, ref: 'Pokemon' }],
     created_at: {
-      type: Date,
-      default: Date.now
+        type: Date,
+        default: Date.now
     }
 });
 
@@ -35,6 +37,16 @@ userSchema.virtual('users', {
     localField: '_id',
     foreignField: 'trainer'
 });
+
+userSchema.virtual('users', {
+    ref: 'User',
+    localField: '_id',
+    foreignField: 'pokemons'
+});
+
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
