59a447fdc8b182ed4ee73bb7152304902f6a37ff
Amélie Barré <amliebarre@gmail.com>
2018-11-26 14:10:41 +0100

    fix access control url

--- src/server.ts
+++ src/server.ts
@@ -42,13 +42,13 @@ class Server {
         this.app.use(function (req, res, next) {
 
             // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.setHeader("Access-Control-Allow-Origin", "*");
 
             // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
             // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
             // Pass to next layer of middleware
             next();
