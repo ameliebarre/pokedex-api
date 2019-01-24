0b94d9e7466db1f94df8b02eafa3a4058b1e2c92
Amélie Barré <amliebarre@gmail.com>
2019-01-24 15:58:16 +0100

    check if token exists

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -5,7 +5,7 @@ export class AuthMiddleware {
     public checkToken = (req, res, next) => {
         let token = req.headers['x-access-token'] || req.headers['authorization'];
 
-        if (token.startsWith('Bearer ')) {
+        if (token && token.startsWith('Bearer ')) {
             // Remove Bearer from string
             token = token.slice(7, token.length).trimLeft();
         }
