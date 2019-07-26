9f8b3fa75bcf24fe9d258f8fe9492e4848a5d5cb
Amélie Barré <amliebarre@gmail.com>
2019-07-26 15:56:08 +0200

    create console controller + method to fetch all consoles

---
+++ src/controllers/console.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+import Console from "../models/Console";
+
+class ConsoleController {
+
+    /**
+     * Get all consoles
+     *
+     * @param {e.Request} req
+     * @param {e.Response} res
+     *
+     * @returns {Promise<void>}
+     */
+    public getAllConsoles(req: Request, res: Response)
+    {
+        try {
+            Console.find({}, (error, consoles) => {
+                if (error) {
+                    throw error;
+                }
+
+                return res.status(200).json(consoles);
+
+            }).populate('games');
+            
+        } catch(error) {
+            res.status(500).send({ message: error.message, success: "false" });
+        }
+    }
+}
+
+export default ConsoleController;
