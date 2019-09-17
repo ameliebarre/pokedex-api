67b6b3fc66d26d862417b09841c64a4433a58ff5
Amélie Barré <amliebarre@gmail.com>
2019-09-17 11:31:18 +0200

    create method to get all the capacities

--- src/controllers/capacity.controller.ts
+++ src/controllers/capacity.controller.ts
@@ -1,6 +1,30 @@
 import { Request, Response } from "express";
 
+import Capacity from "../models/Capacity";
+
 class CapacityController {
+
+    /**
+     * Get all Pokemons
+     *
+     * @param {e.Request} req
+     * @param {e.Response} res
+     */
+    public async getAllCapacities(req: Request, res: Response) {
+        try {
+
+            const populateQuery = [
+                { path: 'type' },
+                { path: 'generation.games' }
+            ];
+
+            const capacities = await Capacity.find({}).populate(populateQuery);
+
+            res.status(200).json(capacities);
+        } catch (error) {
+            res.status(500).send({ message: error.message, success: "false" });
+        }
+    }
 
 }
 
