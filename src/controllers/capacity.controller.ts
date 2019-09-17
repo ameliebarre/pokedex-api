fc01c67362cf07dcc6caf3e95aca3907d100412b
Amélie Barré <amliebarre@gmail.com>
2019-09-17 12:04:07 +0200

    create a method to get a capacity by its slug

--- src/controllers/capacity.controller.ts
+++ src/controllers/capacity.controller.ts
@@ -26,6 +26,36 @@ class CapacityController {
         }
     }
 
+    /**
+     * Get a Capacity by its slug
+     *
+     * @param {e.Request} req
+     * @param {e.Response} res
+     *
+     * @returns {Promise<void>}
+     */
+    public async getOneCapacity(req: Request, res: Response) {
+        try {
+
+            const populateQuery = [
+                { path: 'type' },
+                { path: 'generation.games' }
+            ];
+
+            const capacity = await Capacity.findOne({ 'slug': req.params.id }).populate(populateQuery);
+
+            // If the Pokemon does not exists, send an error
+            if (!capacity) {
+                throw new Error('The capacity does not exist');
+            }
+
+            res.status(200).json(capacity);
+
+        } catch (error) {
+            res.status(500).send({ message: error.message, success: "false" });
+        }
+    }
+
 }
 
 export default CapacityController;
